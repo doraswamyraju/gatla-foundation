025944e17db2d46f3eb0466218ffddb6f6084af9
srinivas-ete <etesrinivas02@gmail.com>
2026-01-21 16:18:01 +0530

    test 3

--- api/process_donation.php
+++ api/process_donation.php
@@ -8,8 +8,8 @@ header("Content-Type: application/json");
 
 // Disable error display to client, enable logging
 // Enable Error Display for Debugging
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
 error_reporting(E_ALL);
 
 function logDebug($msg) {
