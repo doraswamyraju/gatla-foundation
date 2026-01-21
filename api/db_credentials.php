a9f3dec2ef5ea9db6f42840513861fcec12331a7
srinivas-ete <etesrinivas02@gmail.com>
2026-01-21 22:52:29 +0530

    Fix deployment: Track db_env.php (Prod), Ignore db_credentials.php (Local)

--- api/db_credentials.php
+++
@@ -1,8 +0,0 @@
-<?php
-// api/db_credentials.php
-// Local Development Credentials
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'gatla_foundation');
-?>
