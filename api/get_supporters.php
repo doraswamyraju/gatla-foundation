7255a0a9d9dace7d6e6ba8f0eba48b55727861b0
srinivas-ete <etesrinivas02@gmail.com>
2026-01-21 21:43:20 +0530

    cricket

--- api/get_supporters.php
+++ api/get_supporters.php
@@ -11,8 +11,15 @@ $club_filter = isset($_GET['club']) ? $_GET['club'] : '';
 if ($club_filter) {
     // Filter by areas_of_interest using LIKE query
     // NOTE: This is a simple text search. If 'Education' matches 'Education Club', it works.
+    // Customize search term mapping
+    // The form uses "Sports" but the tab uses "Cricket"
+    $search_term = $club_filter;
+    if ($club_filter === 'Cricket') {
+        $search_term = 'Sports';
+    }
+    
     $sql = "SELECT * FROM supporters WHERE areas_of_interest LIKE ? ORDER BY submission_date DESC";
-    $search_term = "%" . $club_filter . "%";
+    $search_term = "%" . $search_term . "%";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("s", $search_term);
     $stmt->execute();
